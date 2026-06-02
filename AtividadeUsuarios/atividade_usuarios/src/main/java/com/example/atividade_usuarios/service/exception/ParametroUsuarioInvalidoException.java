4ade116c59d7a17a331b9f41130ffa610b5998fc
Gabriel Veríssimo <vergabriel07@gmail.com>
2026-06-02 02:15:09 +0000

    Delete AtividadeUsuarios/atividade_usuarios directory

--- AtividadeUsuarios/atividade_usuarios/src/main/java/com/example/atividade_usuarios/service/exception/ParametroUsuarioInvalidoException.java
+++
@@ -1,7 +0,0 @@
-package com.example.atividade_usuarios.service.exception;
-
-public class ParametroUsuarioInvalidoException extends Exception {
-	public ParametroUsuarioInvalidoException(String mensagem) {
-		super("Parametro inválido:\n" + mensagem);
-	}
-}
